0a70c2d6864790aca26c4b180229a079c6f484f3
Gregory Shields <gregoryshields@gregorys-mbp.attlocal.net>
2020-12-17 19:40:34 -0600

    Initial Commit

---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Gregory Shields on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
